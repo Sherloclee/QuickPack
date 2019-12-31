8a5a5f8e4a81662c29e365b5e303a76fda840552
sherlock <sherloc_lee@163.com>
2020-01-01 01:59:18 +0800

    build setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
+from __future__ import print_function
 from setuptools import find_packages, setup
+import sys
+
 
 setup(
     name="QuickPack",
     version="0.0.1",
     author="sherloc_lee",
     author_email="sherloc_lee@163.com",
-    
+    description="Python Utils",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=True
 )
