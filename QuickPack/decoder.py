ab15f4342e20ab1e32384435d0edd475111c5ba5
sherlock <sherloc_lee@163.com>
2020-01-09 23:54:36 +0800

    add decoder and encoder implement

--- QuickPack/decoder.py
+++ QuickPack/decoder.py
@@ -1,1 +1,35 @@
-from itsdangerous import json as _json
+class Decoder:
+    def __init__(self, src=None):
+        self.src = src
+
+    def decode(self, src=None):
+        if self.src is None:
+            return type(None)
+
+    def _functions_decode(self):
+        # todo function decoder
+        pass
+
+    def _int_decode(self):
+        # todo integer decoder
+        pass
+
+    def _str_encode(self):
+        # todo string decoder
+        pass
+
+    def _list_encode(self):
+        # todo list decoder
+        pass
+
+    def _dict_decode(self):
+        pass
+
+    def _tuple_decode(self):
+        pass
+
+    def _set_decode(self):
+        pass
+
+    def _instance_decode(self):
+        pass
