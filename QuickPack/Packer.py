77e9b3ab5d1d0201a388a82033c77dd8377eae5a
sherlock <sherloc_lee@163.com>
2020-01-09 23:53:19 +0800

    define Packer method

--- QuickPack/Packer.py
+++ QuickPack/Packer.py
@@ -1,10 +1,20 @@
+from .decoder import *
+from .encoder import *
+
+
 class Packer(object):
-    def __init__(self, obj):
-        self.obj = obj
-        self.level = 0
+    def __init__(self, obj=None):
+        self.src = obj
 
     def __dump(self, obj):
         pass
 
     def __load(self, data):
+        pass
+
+    def pack(self):
+        if self.src is None:
+            return None
+
+    def unpack(self):
         pass
