bfd15978134219313e42d3cdea4cd031b12270ce
sherlock <sherloc_lee@163.com>
2020-01-09 23:59:39 +0800

    add decoder and encoder implement

--- QuickPack/encoder.py
+++ QuickPack/encoder.py
@@ -1,5 +1,37 @@
-from itsdangerous import json as _json
+class Encoder:
+    def encode(self) -> (bytes, bytes):
+        """
+        :return:(description bytes, data_bytes)
+        """
+        pass
 
+    def _functions_encode(self) -> (bytes, bytes):
+        # todo function encoder
+        pass
 
-class JsonEncoder(_json.JSONEncoder):
-    pass
+    def _long_encode(self):
+        pass
+
+    def _int_encode(self):
+        # todo integer encoder
+        pass
+
+    def _str_encode(self):
+        # todo string encoder
+        pass
+
+    def _list_encode(self):
+        # todo list encoder
+        pass
+
+    def _dict_encode(self):
+        pass
+
+    def _tuple_encode(self):
+        pass
+
+    def _set_encode(self):
+        pass
+
+    def _instance_encode(self):
+        pass
